a90c84027fe45aabaf7c177d40de79501a914432
Phcode007 <paulobelarmino047@gmail.com>
2025-10-27 15:55:38 -0300

    Implementação da função login

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import Navbar from "./components/navbar/Navbar";
 import Cadastro from "./pages/cadastro/Cadastro";
 import Footer from "./components/footer/Footer";
@@ -11,8 +12,9 @@ function App() {
         <Navbar />
         <div className="min-h-[80vh]">
           <Routes>
-            <Route path="/" element={<Cadastro />} />
+            <Route path="/" element={<Login />} />
             <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
           </Routes>
         </div>
         <Footer />
