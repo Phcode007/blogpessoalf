2054441f618a9c04460ce35e880e747790bd1c7e
Phcode007 <paulobelarmino047@gmail.com>
2025-10-27 11:23:03 -0300

    Criação e implementação da página de Cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/Navbar";
+import Cadastro from "./pages/cadastro/Cadastro";
 import Footer from "./components/footer/Footer";
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
         <Navbar />
         <div className="min-h-[80vh]">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Cadastro />} />
             <Route path="/home" element={<Home />} />
           </Routes>
         </div>
