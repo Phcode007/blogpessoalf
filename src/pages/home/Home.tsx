b9dc47488b309b138eb0eaa5aa5650531bb6f38e
Phcode007 <paulobelarmino047@gmail.com>
2025-10-30 15:10:27 -0300

    Componentes CardPostagem e ListaPostagens - Lógica

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,19 +1,17 @@
+import ListaPostagens from "../../components/postagem/listapostagem/ListaPostagem";
+
 function Home() {
   return (
     <>
       <div className="bg-indigo-900 flex justify-center">
-        <div className='container grid grid-cols-2 text-white'>
+        <div className="container grid grid-cols-2 text-white">
           <div className="flex flex-col gap-4 items-center justify-center py-4">
-            <h2 className='text-5xl font-bold'>
-              Seja Bem Vinde!
-            </h2>
+            <h2 className="text-5xl font-bold">Seja Bem Vinde!</h2>
 
-            <p className='text-xl'>
-              Expresse aqui seus pensamentos e opiniões
-            </p>
+            <p className="text-xl">Expresse aqui seus pensamentos e opiniões</p>
 
             <div className="flex justify-around gap-4">
-              <div className='rounded text-white border-white border-solid border-2 py-2 px-4'>
+              <div className="rounded text-white border-white border-solid border-2 py-2 px-4">
                 Nova Postagem
               </div>
             </div>
@@ -23,13 +21,14 @@ function Home() {
             <img
               src="https://i.imgur.com/fyfri1v.png"
               alt="Imagem Página Home"
-              className='w-2/3'
+              className="w-2/3"
             />
           </div>
         </div>
       </div>
+      <ListaPostagens />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
