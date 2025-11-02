19bdd2ee0114152c50212b743e5e4f8589851b44
Phcode007 <paulobelarmino047@gmail.com>
2025-11-02 11:55:23 -0300

    Implementação do ToastALerta no App.tsx, AuthContext e Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,13 @@ import ListaPostagens from "./components/postagem/listapostagem/ListaPostagem";
 import FormPostagem from "./components/postagem/formpostagem/FormPostagem";
 import DeletarPostagem from "./components/postagem/deletarpostagem/DeletarPostagem";
 import Perfil from "./pages/perfil/Perfil";
-
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 function App() {
   return (
     <>
       <AuthProvider>
+        <ToastContainer />
         <BrowserRouter>
           <Navbar />
           <div className="min-h-[80vh]">
